6728a89bdff0f8f6a557e87289b7b0d3a36699d8
Jim Peeters <Jim Peeters>
2016-04-18 19:06:50 +0200

    Front-end : Images tijdelijk toegevoegd + loops added

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,6 +4,11 @@
 
 @section('content')
 
+	<div class="title-section" style="background-image: url('/images/register.jpg');">
+		<h1 ng-show="!loginView">Register</h1>
+		<h1 ng-show="loginView">Login</h1>
+	</div>
+
 	@if (count($errors) > 0)
 	    <div class="col-xs-12 col-md-6 col-md-offset-3" ng-controller="AlertController">
 	    	<div class="info-box error" ng-hide="hidden" ng-class="{fade: startFade}">
@@ -16,14 +21,6 @@
 			</div>
 	    </div>
 	@endif
-
-	<div ng-show="!loginView" class="col-xs-12 col-md-6 col-md-offset-3">
-		<h2 class="block-title">Register</h2>
-	</div>
-
-	<div ng-show="loginView" class="col-xs-12 col-md-6 col-md-offset-3">
-		<h2 class="block-title">Login</h2>
-	</div>
 
 	<div ng-init="loginView = false">
 		<div ng-show="!loginView" class="col-xs-12 col-md-6 col-md-offset-3">
