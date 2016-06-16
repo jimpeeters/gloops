bc9347b242c46ab52292900887000e31f5dbd62d
Jim Peeters <Jim Peeters>
2016-06-16 16:52:48 +0200

    Front-end : videos added to gloops

--- resources/views/snippets/videos-modal.blade.php
+++ resources/views/snippets/videos-modal.blade.php
@@ -1,32 +1,26 @@
 <div id="video-modal" class="modal-bottom bottom-sheet">
 	<div class="modal-bottom-content">
         <h4 class="video-modal-title">GLOOPS VIDEOS</h4>
-         <ul class="collection">
-	        <li class="collection-item avatar">
-	            <img src="/images/videoThumbnails/tutorial1.png" alt="A screenshot of the youtube video" class="circle">
-	            <p class="collection-title">How to make loops with audacity?</p>
-	            <p class="collection-info">This tutorial teaches you how to record and upload loops with audacity. In only 3 minutes you will be able to add guitar loops to your account!</p>
-	            <a target="_blank" href="https://www.youtube.com/watch?v=TwhiN12jhdo" class="secondary-content youtube-link"><i class="fa fa-3x fa-youtube-play" aria-hidden="true"></i></a>
-	        </li>
-<!-- 	        <li class="collection-item avatar">
-    <img src="images/profilePictures/admin.jpg" alt="A screenshot of the youtube video" class="circle">
-    <p class="collection-title">Title video 1</p>
-    <p class="collection-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptatibus molestiae expedita omnis quam dignissimos rerum.</p>
-    <a href="#!" class="secondary-content youtube-link"><i class="fa fa-3x fa-youtube-play" aria-hidden="true"></i></a>
-</li>
-<li class="collection-item avatar">
-    <img src="images/profilePictures/admin.jpg" alt="A screenshot of the youtube video" class="circle">
-    <p class="collection-title">Title video 1</p>
-    <p class="collection-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptatibus molestiae expedita omnis quam dignissimos rerum.</p>
-    <a href="#!" class="secondary-content youtube-link"><i class="fa fa-3x fa-youtube-play" aria-hidden="true"></i></a>
-</li>
-<li class="collection-item avatar">
-    <img src="images/profilePictures/admin.jpg" alt="A screenshot of the youtube video" class="circle">
-    <p class="collection-title">Title video 1</p>
-    <p class="collection-info">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo voluptatibus molestiae expedita omnis quam dignissimos rerum.</p>
-    <a href="#!" class="secondary-content youtube-link"><i class="fa fa-3x fa-youtube-play" aria-hidden="true"></i></a>
-</li> -->
-</ul>
+            <ul class="collection">
+                <li class="collection-item avatar">
+                    <img src="/images/videoThumbnails/thumbnail-main.png" alt="A screenshot of gloops on a smartphone" class="circle">
+                    <a class="collection-title" target="_blank" href="https://www.youtube.com/watch?v=2MKaWri-h18">What is gloops?</a>
+                    <p class="collection-info">What is gloops? Watch this demonstration video to show you around.</p>
+                    <a target="_blank" href="https://www.youtube.com/watch?v=2MKaWri-h18" class="secondary-content youtube-link"><i class="fa fa-3x fa-youtube-play" aria-hidden="true"></i></a>
+                </li>
+                <li class="collection-item avatar">
+                    <img src="/images/videoThumbnails/tutorial1.png" alt="A screenshot of the youtube video : tutorial 1" class="circle">
+                    <a class="collection-title" target="_blank" href="https://www.youtube.com/watch?v=TwhiN12jhdo">How to make loops with audacity?</a>
+                    <p class="collection-info">This tutorial teaches you how to record and upload loops with audacity. In only 3 minutes you will be able to add guitar loops to your account!</p>
+                    <a target="_blank" href="https://www.youtube.com/watch?v=TwhiN12jhdo" class="secondary-content youtube-link"><i class="fa fa-3x fa-youtube-play" aria-hidden="true"></i></a>
+                </li>
+                <li class="collection-item avatar">
+                    <img src="/images/videoThumbnails/tutorial2.png" alt="A screenshot of the youtube video : tutorial 2" class="circle">
+                    <a class="collection-title" target="_blank" href="https://www.youtube.com/watch?v=2hUTcuSJ-xE">What is a way to jam on a guitar loop?</a>
+                    <p class="collection-info">Do you have no clue how to jam on guitar loops or backing tracks? This video can give u a quick hint on how to be able to jam on loops.</p>
+                    <a target="_blank" href="https://www.youtube.com/watch?v=2hUTcuSJ-xE" class="secondary-content youtube-link"><i class="fa fa-3x fa-youtube-play" aria-hidden="true"></i></a>
+                </li>
+            </ul>
     </div>
 </div>
           
